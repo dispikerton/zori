731bc9b1f26b8ceb683f0e23dc5f357066982f5f
Dmitry <dispikerton@gmail.com>
2021-04-18 10:48:47 +0300

    remade search technology methods

--- src/main/java/com/zolotenkov/spring/springboot/zori_zavod/accounting_production_operations/repository/TechnologyRepository.java
+++ src/main/java/com/zolotenkov/spring/springboot/zori_zavod/accounting_production_operations/repository/TechnologyRepository.java
@@ -3,5 +3,10 @@ package com.zolotenkov.spring.springboot.zori_zavod.accounting_production_operat
 import com.zolotenkov.spring.springboot.zori_zavod.accounting_production_operations.entity.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+    Technology findByIdAndIsDeletedFalse(Long id);
+
+    List<Technology> findAllByIsDeletedFalse();
 }
--- src/main/java/com/zolotenkov/spring/springboot/zori_zavod/accounting_production_operations/rest/TechnologyRestController.java
+++ src/main/java/com/zolotenkov/spring/springboot/zori_zavod/accounting_production_operations/rest/TechnologyRestController.java
@@ -29,48 +29,44 @@ public class TechnologyRestController {
 
     @GetMapping("/tech/{id}/operation/all")
     private ResponseEntity<List<Operation>> allOperationsByTechnology(@PathVariable Long id){
-        Technology technology = technologyRepository.findById(id).get();
+        Technology technology = technologyRepository.findByIdAndIsDeletedFalse(id);
         List<Operation> list = operationRepository.findAllByTechnologyAndAndIsDeletedFalse(technology);
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/tech/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    private ResponseEntity<List<Technology>> getAllTechnologies (){
+        List<Technology> list = technologyRepository.findAllByIsDeletedFalse();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/tech/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    private ResponseEntity<Technology> getTechnologyById (@PathVariable Long id){
+        Technology technology = technologyRepository.findByIdAndIsDeletedFalse(id);
+        return new ResponseEntity<>(technology, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/tech/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    private ResponseEntity<Technology> saveTechnology(@RequestBody Technology technology){
+        if (technology == null)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        technologyRepository.save(technology);
+        return new ResponseEntity<>(technology, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/operation/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    private ResponseEntity<Operation> saveOperation(@RequestBody Operation operation){
+        if (operation == null)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        this.operationRepository.save(operation);
+        return new ResponseEntity<>(operation, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/tech/delete/{techId}")
     private ResponseEntity<Technology> deleteTechnology(@PathVariable Long techId) {
         technologyRepository.deleteById(techId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PostMapping(value = "/tech/add", produces = MediaType.APPLICATION_JSON_VALUE)
-    private ResponseEntity<Technology> saveTechnology(@RequestBody Technology technology){
-        HttpHeaders headers = new HttpHeaders();
-
-        if (technology == null)
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
-        technologyRepository.save(technology);
-        return new ResponseEntity<>(technology, headers, HttpStatus.CREATED);
-    }
-
-    @PostMapping(value = "/operation/add", produces = MediaType.APPLICATION_JSON_VALUE)
-    private ResponseEntity<Operation> saveOperation(@RequestBody Operation operation){
-        HttpHeaders headers = new HttpHeaders();
-
-        if (operation == null)
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
-        this.operationRepository.save(operation);
-        return new ResponseEntity<>(operation, headers, HttpStatus.CREATED);
-    }
-
-    @GetMapping(value = "/tech/all", produces = MediaType.APPLICATION_JSON_VALUE)
-    private ResponseEntity<List<Technology>> getAllTechnologies (){
-        List<Technology> list = technologyRepository.findAll();
-        return new ResponseEntity<>(list, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/tech/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    private ResponseEntity<Technology> getTechnologyById (@PathVariable Long id){
-        Technology technology = technologyRepository.findById(id).get();
-        return new ResponseEntity<>(technology, HttpStatus.OK);
     }
 }
